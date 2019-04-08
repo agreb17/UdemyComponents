2badae38f8aabdb2186dac6ad5108009e10ec51f
Anthony Greb <agreb255@gmail.com>
2019-04-08 14:45:20 -0400

    cleaned reactDOM error

--- components/src/index.js
+++ components/src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hello</div>
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                <img alt="avatar" />
+                </a>
+                <div className="content">
+                <a href="/" className="author">
+                Anthony
+                </a>
+                <div className="metadata">
+                    <span className="date">Today @ 6:00pm</span>
+                </div>
+                <div className="text">
+                Nice blog post
+                </div>
+                </div>
+            </div>
+        </div>
+    );
 
 };
 
