3847dbb6918fc3000def12eeeceac8c035de5aa2
Anthony Greb <agreb255@gmail.com>
2019-04-08 18:11:27 -0400

    added cards

--- components/src/index.js
+++ components/src/index.js
@@ -7,8 +7,37 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
     return (
         <div className="ui container comments">
-        <ApprovalCard />
-            <CommentDetail name="Natalie" timeAt="Today @6:53pm" content="Very Nice!" faker={faker.image.avatar()}/>
+        <ApprovalCard>
+            <div>
+                <h2>Warning!</h2>
+                <h4>Are you sure you want to do this?</h4>
+            </div>
+            
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail 
+                name="Anthony" 
+                timeAt="Today @6:53pm" 
+                content="Very Nice!" 
+                faker={faker.image.avatar()}
+            />
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail 
+                name="Natalie" 
+                timeAt="Today @6:55pm" 
+                content="Your mom's very nice..." 
+                faker={faker.image.avatar()}
+            />
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail 
+                name="Thomas" 
+                timeAt="Today @6:59pm" 
+                content="Hahahaha" 
+                faker={faker.image.avatar()}
+            />
+        </ApprovalCard>
         </div>
     );
 
